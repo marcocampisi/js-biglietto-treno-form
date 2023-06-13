8d2f14c2d0ea9a481c0f7a6293bf5ea116e0fba6
marcocampisi <marco.campisi@outlook.it>
2023-06-13 14:45:34 +0200

    Added form and base values

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 const tariffaKm = 0.21;
+const inputFullName = document.getElementById('inputFullName').value;
+const inputKm = parseInt(document.getElementById('inputKm').value);
+const inputAge = parseInt(document.getElementById('inputAge').value);
+const generateBtn = document.getElementById('generateBtn');
+
+generateBtn.addEventListener('click',
+    function() {
+        console.log(inputFullName, inputKm, inputAge)
+    }
+)
