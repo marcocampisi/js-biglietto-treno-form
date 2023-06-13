4f367c993ad6120199868008cf19c0168302718a
marcocampisi <marco.campisi@outlook.it>
2023-06-13 15:46:47 +0200

    Fixed final price calculation

--- js/main.js
+++ js/main.js
@@ -2,19 +2,27 @@ const tariffaKm = 0.21;
 const generateBtn = document.getElementById('generateBtn');
 const resetBtn = document.getElementById('resetBtn');
 const costoBiglietto = document.getElementById('costoBiglietto');
+const nameField = document.getElementById('nameField');
+const promoField = document.getElementById('promoField')
 
 generateBtn.addEventListener('click',
     function() {
         const inputFullName = document.getElementById('inputFullName').value;
-        const inputKm = parseInt(document.getElementById('inputKm').value);
+        const inputKm = parseFloat(document.getElementById('inputKm').value);
         const inputAge = document.getElementById('inputAge').value;
         let prezzo = parseFloat((tariffaKm * inputKm).toFixed(2));
+
+        nameField.innerHTML = inputFullName;
+
         if (inputAge == 'under18') {
             costoBiglietto.innerHTML = ((prezzo) - (prezzo * .20)) + '€';
+            promoField.innerHTML = 'Biglietto ridotto minorenni';
         } else if (inputAge == 'over65') {
             costoBiglietto.innerHTML = ((prezzo) - (prezzo * .40)) + '€';
+            promoField.innerHTML - 'Biglietto ridotto anziani';
         } else {
             costoBiglietto.innerHTML = prezzo + '€';
+            promoField.innerHTML = 'Biglietto standard';
         }
     }
 );
