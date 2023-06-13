0a7908894770958fb836dcb2911b4f6f833b3fad
marcocampisi <marco.campisi@outlook.it>
2023-06-13 15:31:55 +0200

    Added price calc function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 const tariffaKm = 0.21;
-const inputFullName = document.getElementById('inputFullName').value;
-const inputKm = parseInt(document.getElementById('inputKm').value);
-const inputAge = document.getElementById('inputAge').value;
 const generateBtn = document.getElementById('generateBtn');
 const resetBtn = document.getElementById('resetBtn');
+const costoBiglietto = document.getElementById('costoBiglietto');
 
 generateBtn.addEventListener('click',
     function() {
-        console.log(inputFullName, inputKm, inputAge)
+        const inputFullName = document.getElementById('inputFullName').value;
+        const inputKm = parseInt(document.getElementById('inputKm').value);
+        const inputAge = document.getElementById('inputAge').value;
+        let prezzo = parseFloat((tariffaKm * inputKm).toFixed(2));
+        if (inputAge == 'under18') {
+            costoBiglietto.innerHTML = ((prezzo) - (prezzo * .20)) + '€';
+        } else if (inputAge == 'over65') {
+            costoBiglietto.innerHTML = ((prezzo) - (prezzo * .40)) + '€';
+        } else {
+            costoBiglietto.innerHTML = prezzo + '€';
+        }
     }
-)
+);
 
 resetBtn.addEventListener('click',
     function() {
-        document.querySelector('form').reset()
+        document.querySelector('form').reset();
     }
-)
+);
