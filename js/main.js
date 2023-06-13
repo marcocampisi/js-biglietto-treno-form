535428ac5122efe4c5a1c7f280515a0de20db6c5
marcocampisi <marco.campisi@outlook.it>
2023-06-13 14:55:50 +0200

    Added base form function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const tariffaKm = 0.21;
 const inputFullName = document.getElementById('inputFullName').value;
 const inputKm = parseInt(document.getElementById('inputKm').value);
-const inputAge = parseInt(document.getElementById('inputAge').value);
+const inputAge = document.getElementById('inputAge').value;
 const generateBtn = document.getElementById('generateBtn');
+const resetBtn = document.getElementById('resetBtn');
 
 generateBtn.addEventListener('click',
     function() {
         console.log(inputFullName, inputKm, inputAge)
+    }
+)
+
+resetBtn.addEventListener('click',
+    function() {
+        document.querySelector('form').reset()
     }
 )
